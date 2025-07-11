dc20019fdc1869e10dcfccae2b96729973ebd9f1
EsMASTERCHIEF <josemorales11756@gmail.com>
2025-07-11 17:24:20 +0000

    Update

--- Streamlit.py
+++ Streamlit.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-pipeline_path = r"/workspaces/R2/artefacts/preprocessor/preprocessor.pkl"
-model_path = r"/workspaces/R2/artefacts/model/svc.pkl"
-encoder_path = r"/workspaces/R2/artefacts/preprocessor/labelencoder.pkl"
+pipeline_path = "/workspaces/R2/artefacts/preprocessor/preprocessor.pkl"
+model_path = "/workspaces/R2/artefacts/model/svc.pkl"
+encoder_path = "/workspaces/R2/artefacts/preprocessor/labelencoder.pkl"
 with open(pipeline_path, 'rb') as file1:
     print(file1.read(100))
 
